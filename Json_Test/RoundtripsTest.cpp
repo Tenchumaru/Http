26a5fea49608693fc1fe6abf484db2896cf1b49c
Chris <cidzerda@sbcglobal.net>
2018-04-28 21:35:04 -0700

    Test for some round-trips

---
+++ Json_Test/RoundtripsTest.cpp
@@ -0,0 +1,24 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace Json_Test {
+	TEST_CLASS(RoundtripsTest) {
+public:
+
+	TEST_METHOD(Roundtrips) {
+		std::ifstream testCases("../../Json_test/roundtrips.json.txt");
+		if(!testCases) {
+			throw std::runtime_error("cannot find roundtrips.json.txt");
+		}
+		std::string line;
+		while(std::getline(testCases, line)) {
+			auto json= Parser::Parse(line.c_str());
+			std::stringstream ss;
+			json.Write(ss);
+			Assert::AreEqual(line, ss.str());
+		}
+	}
+	};
+}
