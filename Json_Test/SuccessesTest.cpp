682270d5d44e2743b69be7fc16d0316d44e24e85
Chris <cidzerda@sbcglobal.net>
2018-04-28 19:24:00 -0700

    Add more success test cases

--- Json_Test/SuccessesTest.cpp
+++ Json_Test/SuccessesTest.cpp
@@ -7,6 +7,24 @@ namespace Json_Test {
 	TEST_CLASS(SuccessesTest) {
 public:
 
+	TEST_METHOD(Successes) {
+		std::ifstream testCases("../../Json_test/successes.json.txt");
+		if(!testCases) {
+			throw std::runtime_error("cannot find successes.json.txt");
+		}
+		std::string line, text;
+		while(std::getline(testCases, line)) {
+			if(line == "-------") {
+				Parser::Parse(text.c_str());
+				text.clear();
+			} else {
+				text += '\n';
+				text += line;
+			}
+		}
+		Parser::Parse(text.c_str());
+	}
+
 	TEST_METHOD(SuccessesGoogleCalendar) {
 		std::ifstream testCases("../../Json_test/google-calendar.json");
 		if(!testCases) {
