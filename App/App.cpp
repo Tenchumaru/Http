c6897bf760d19e5e1353c2a2fa1e4618ad35e223
Chris <cidzerda@sbcglobal.net>
2018-05-04 21:05:38 -0700

    Implement a plain-text endpoint

--- App/App.cpp
+++ App/App.cpp
@@ -12,9 +12,16 @@ void HandleJson(Request const& request, Response& response) {
 	response.Ok(ss.str());
 }
 
+void HandlePlainText(Request const& request, Response& response) {
+	request, response;
+	response.Headers.insert({ "Content-Type", "text/plain" });
+	response.Ok("Hello, World!");
+}
+
 int main() {
 	HttpServer server;
 	server.Add("/json", HandleJson);
+	server.Add("/plaintext", HandlePlainText);
 	server.Listen(6006);
 	return 0;
 }
