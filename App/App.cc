67605ed5bca04ca673789fa2de22b26c0bbd0558
Chris <github@adrezdi.com>
2018-10-24 20:05:23 -0700

    Add Access Control headers

--- App/App.cc
+++ App/App.cc
@@ -2,16 +2,20 @@
 #include "../Json/Json.h"
 #include "../Http/HttpServer.h"
 
-void HandleJson(Request const& /*request*/, Response& response) {
+void HandleJson(Request const& request, Response& response) {
 	std::stringstream ss;
 	std::unordered_map<std::string, std::string> object;
 	object.insert({ "message", "Hello, World!" });
 	WriteJson(ss, object);
+	response.Headers.insert({ "Access-Control-Allow-Credentials", "true" });
+	response.Headers.insert({ "Access-Control-Allow-Origin", request.Headers.at("origin") });
 	response.Headers.insert({ "Content-Type", "application/json" });
 	response.Ok(ss.str());
 }
 
-void HandlePlainText(Request const& /*request*/, Response& response) {
+void HandlePlainText(Request const& request, Response& response) {
+	response.Headers.insert({ "Access-Control-Allow-Credentials", "true" });
+	response.Headers.insert({ "Access-Control-Allow-Origin", request.Headers.at("origin") });
 	response.Headers.insert({ "Content-Type", "text/plain" });
 	response.Ok("Hello, World!");
 }
