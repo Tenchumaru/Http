cc07dca903d292d55345352187e60ad8f95c36dd
Chris <github@adrezdi.com>
2019-01-04 20:23:37 -0800

    Keep transitions in the DfaState structure

--- mkdispatch/SmPrinter.cpp
+++ mkdispatch/SmPrinter.cpp
@@ -20,17 +20,17 @@ namespace {
 
 		static ptr Create(Printer::vector const& requests) {
 			ptr startState = std::make_shared<NfaState>(0);
-			std::for_each(requests.cbegin(), requests.cend(), [startState](Printer::Request const& request) {
+			for(Printer::Request const& request : requests) {
 				ptr currentState = startState;
-				std::for_each(request.line.cbegin(), request.line.cend(), [&currentState](char ch) {
+				for(char ch : request.line) {
 					currentState = currentState->PushState(ch);
 					if(ch == ':') {
 						currentState->transitions.push_back({ ch, currentState });
 					}
-				});
+				}
 				currentState = currentState->PushState('\n');
 				currentState->fn = request.fn;
-			});
+			}
 			return startState;
 		}
 
@@ -44,68 +44,71 @@ namespace {
 	size_t NfaState::next;
 
 	struct DfaState {
-		using map = std::map<std::string, std::map<char, DfaState>>;
+		using map = std::map<char, DfaState>;
 		using vector = std::vector<NfaState::ptr>;
 
 		vector nfaStates;
+		map transitions;
 		bool isMarked;
 		__declspec(property(get = get_Name)) std::string Name;
 		__declspec(property(get = get_Symbols)) std::set<char> Symbols;
 
 		std::string get_Name() const {
 			std::set<size_t> numbers;
-			std::for_each(nfaStates.cbegin(), nfaStates.cend(), [&numbers](NfaState::ptr state) {
+			for(NfaState::ptr state : nfaStates) {
 				numbers.insert(state->number);
-			});
+			}
 			std::ostringstream ss;
-			std::for_each(numbers.cbegin(), numbers.cend(), [&ss](size_t number) {
+			for(size_t number : numbers) {
 				ss << ',' << number;
-			});
+			}
 			return ss.str().substr(1);
 		}
 
 		std::set<char> get_Symbols() const {
 			std::set<char> rv;
-			std::for_each(nfaStates.cbegin(), nfaStates.cend(), [&rv](NfaState::ptr state) {
-				std::for_each(state->transitions.cbegin(), state->transitions.cend(), [&rv](NfaState::pair const& pair) {
+			for(NfaState::ptr state : nfaStates) {
+				for(NfaState::pair const& pair : state->transitions) {
 					rv.insert(pair.first);
-				});
-			});
+				}
+			}
 			return rv;
 		}
 
 		DfaState Move(char ch) {
 			DfaState rv{};
-			std::for_each(nfaStates.cbegin(), nfaStates.cend(), [ch, &rv](NfaState::ptr state) {
-				std::for_each(state->transitions.cbegin(), state->transitions.cend(), [ch, &rv](NfaState::pair const& pair) {
+			for(NfaState::ptr state : nfaStates) {
+				for(NfaState::pair const& pair : state->transitions) {
 					if(pair.first == ch || (pair.first == ':' && ch != '\n')) {
 						rv.nfaStates.push_back(pair.second);
 					}
-				});
-			});
+				}
+			}
 			return rv;
 		}
 
-		static map From(NfaState::ptr nfaStartState) {
+		static std::map<std::string, DfaState> From(NfaState::ptr nfaStartState) {
 			// p. 118
-			std::map<std::string, DfaState> dstates({ { "0", DfaState{ { nfaStartState } } } });
-			map dtran;
+			std::map<std::string, DfaState> states({ { "0", DfaState{ { nfaStartState } } } });
 			for(;;) {
-				auto it = std::find_if(dstates.begin(), dstates.end(), [](decltype(dstates)::value_type& pair) { return !pair.second.isMarked; });
-				if(it == dstates.cend()) {
+				auto it = std::find_if(states.begin(), states.end(), [](decltype(states)::value_type& pair) { return !pair.second.isMarked; });
+				if(it == states.cend()) {
 					break;
 				}
 				auto& t = it->second;
 				t.isMarked = true;
 				for(char a : t.Symbols) {
 					auto u = t.Move(a);
-					if(dstates.find(u.Name) == dstates.cend()) {
-						dstates.insert({ u.Name, u });
+					auto state = states.find(u.Name);
+					if(state == states.cend()) {
+						states.insert({ u.Name, u });
+						t.transitions.insert({ a, u });
+					} else {
+						t.transitions.insert({ a, state->second });
 					}
-					dtran[t.Name].insert({ a, u });
 				}
 			}
-			return dtran;
+			return states;
 		}
 	};
 }
@@ -116,7 +119,7 @@ SmPrinter::~SmPrinter() {}
 
 void SmPrinter::InternalPrint(vector const& requests, Options const& options) {
 	options;
-	
+
 	// Determine the maximum number of parameters in a request.
 	ptrdiff_t nparameters = 0;
 	for(Printer::Request const& request : requests) {
@@ -127,79 +130,57 @@ void SmPrinter::InternalPrint(vector const& requests, Options const& options) {
 	NfaState::ptr nfaStartState = NfaState::Create(requests);
 
 	// Convert the NFA into a DFA.
-	DfaState::map transitions = DfaState::From(nfaStartState);
+	auto dfaStates = DfaState::From(nfaStartState);
 
 	// Map DFA state names to sequential numbers and vice versa.
 	std::map<std::string, int> stateNumbers{ { "0", 0 } };
 	std::vector<std::string> stateNames{ "0" };
 	int next = 0;
-	std::for_each(transitions.cbegin(), transitions.cend(), [&next, &stateNames, &stateNumbers](DfaState::map::value_type const& pair) {
+	for(auto& pair : dfaStates) {
 		if(stateNumbers.find(pair.first) == stateNumbers.cend()) {
 			stateNumbers.insert({ pair.first, ++next });
 			stateNames.push_back(pair.first);
 		}
-		std::for_each(pair.second.cbegin(), pair.second.cend(), [&next, &stateNames, &stateNumbers](std::pair<char, DfaState> const& pair) {
-			if(stateNumbers.find(pair.second.Name) == stateNumbers.cend()) {
-				stateNumbers.insert({ pair.second.Name, ++next });
-				stateNames.push_back(pair.second.Name);
-			}
-		});
-	});
+	}
 
 	// Print the function array.
 	std::map<std::string, size_t> fnIndices;
 	std::cout << "\tstatic void* fns[]= {" << std::endl;
-	std::for_each(requests.cbegin(), requests.cend(), [&fnIndices](Request const& request) {
+	for(Request const& request : requests) {
 		std::cout << "\t\t&" << request.fn << ", // " << fnIndices.size() << std::endl;
 		fnIndices.insert({ request.fn, fnIndices.size() });
-	});
+	}
 	std::cout << "};" << std::endl;
 
 	// Print the state machine.
-	std::set<size_t> wasPrinted;
-	auto const* type = stateNames.size() < 128 ? "char" : stateNames.size() < 32768 ? "short" : "int";
+	auto const* type = stateNumbers.size() < 128 ? "char" : stateNumbers.size() < 32768 ? "short" : "int";
 	std::cout << "\tstatic " << type << " states[" << stateNumbers.size() << "][256] = {" << std::endl;
-	for(size_t i = 0; i < stateNames.size(); ++i) {
-		for(DfaState::map::value_type const& transition : transitions) {
-			if(stateNumbers[transition.first] == i) {
-				if(!wasPrinted.insert(i).second) {
-					std::cerr << "unexpected state " << i << std::endl;
-				}
-				std::vector<int> states(256, 0);
-				for(std::pair<char, DfaState> const& pair : transition.second) {
-					if(pair.first == '\n') {
-						states['\n'] = -stateNumbers[pair.second.Name];
-					} else if(pair.first == ':') {
-						for(char ch : chars) {
-							states[ch] = stateNumbers[pair.second.Name];
-						}
-					} else {
-						states[pair.first] = stateNumbers[pair.second.Name];
+	for(size_t i = 0, n = stateNumbers.size(); i < n; ++i) {
+		std::cout << "\t\t{";
+		auto const& dfaState = dfaStates[stateNames[i]];
+		if(dfaState.transitions.empty()) {
+			auto const& state = *dfaState.nfaStates.cbegin();
+			std::cout << fnIndices[state->fn];
+		} else {
+			std::vector<int> states(256, 0);
+			for(std::pair<char, DfaState> const& pair : dfaState.transitions) {
+				int stateNumber = stateNumbers[pair.second.Name];
+				if(pair.first == '\n') {
+					states['\n'] = -stateNumber;
+				} else if(pair.first == ':') {
+					for(char ch : chars) {
+						states[ch] = stateNumber;
 					}
-				}
-				states.erase(std::find_if(states.crbegin(), states.crend(), [](int state) { return state != 0; }).base(), states.cend());
-				std::cout << "\t\t{";
-				for(int state : states) {
-					std::cout << state << ',';
-				}
-				std::cout << "}, // " << i << std::endl;
-			}
-		}
-		if(wasPrinted.find(i) == wasPrinted.cend()) {
-			for(DfaState::map::value_type const& transition : transitions) {
-				for(std::pair<char, DfaState> const& pair : transition.second) {
-					if(stateNumbers[pair.second.Name] == i) {
-						if(!wasPrinted.insert(i).second) {
-							std::cerr << "unexpected state " << i << std::endl;
-						}
-						std::cout << "\t\t{";
-						auto const& state = *pair.second.nfaStates.cbegin();
-						std::cout << fnIndices[state->fn];
-						std::cout << "}, // " << i << std::endl;
-					}
+				} else {
+					states[pair.first] = stateNumber;
 				}
 			}
+			states.erase(std::find_if(states.crbegin(), states.crend(), [](int state) { return state != 0; }).base(), states.cend());
+			for(int state : states) {
+				std::cout << state << ',';
+			}
 		}
+		std::cout << "}, // " << i << std::endl;
 	}
 	std::cout << "\t};" << std::endl;
 }
