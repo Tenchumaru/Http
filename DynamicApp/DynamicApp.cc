735d90968789a5ef627ab8f0e397c0f0a12e5e28
Tenchumaru <4085772+Tenchumaru@users.noreply.github.com>
2019-06-23 09:39:26 -0700

    Add the DynamicApp project to the solution
    This application uses a mapping of routes to functions.

---
+++ DynamicApp/DynamicApp.cc
@@ -0,0 +1,49 @@
+#include "stdafx.h"
+#include "../Json/Json.h"
+#include "../Http/Http.h"
+#include "../Http/DynamicHttpServer.h"
+
+void HandleJson(Request const& request, Response& response) {
+	std::stringstream ss;
+	std::unordered_map<std::string, std::string> object;
+	object.insert({ "message", "Hello, World!" });
+	WriteJson(ss, object);
+	response.WriteStatus(StatusLines::OK);
+	auto it = request.Headers.find("origin");
+	if(it != request.Headers.cend()) {
+		response.WriteHeader("Access-Control-Allow-Credentials", "true");
+		response.WriteHeader("Access-Control-Allow-Origin", it->second);
+	}
+	response.WriteHeader("Content-Type", "application/json");
+	response << ss.str();
+}
+
+void HandlePlainText(Request const& request, Response& response) {
+	auto it = request.Headers.find("origin");
+	if(it != request.Headers.cend()) {
+		response.WriteHeader("Access-Control-Allow-Credentials", "true");
+		response.WriteHeader("Access-Control-Allow-Origin", it->second);
+	}
+	response.WriteHeader("Content-Type", "text/plain");
+	response << "Hello, World!";
+}
+
+int main(int argc, char* argv[]) {
+	char const* prog = strrchr(argv[0], '\\');
+	if(prog) {
+		++prog;
+	} else {
+		prog = argv[0];
+	}
+	DynamicHttpServer server;
+	if(argc == 3) {
+		server.ConfigureSecurity(argv[1], argv[2]);
+	} else if(argc != 1) {
+		std::cout << "usage: " << prog << " [certificateChainFile privateKeyFile]" << std::endl;
+		return 2;
+	}
+	server.Add("/json", HandleJson);
+	server.Add("/plaintext", HandlePlainText);
+	server.Listen(6006);
+	return 0;
+}
