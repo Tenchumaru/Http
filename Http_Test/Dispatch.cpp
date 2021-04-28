0f57612f10e9a093907cafc6ce7ed7e7f4c37104
Tenchumaru <4085772+Tenchumaru@users.noreply.github.com>
2021-04-28 09:20:35 -0700

    Add DispatchAsync function to Http_Test project

--- Http_Test/Dispatch.cpp
+++ Http_Test/Dispatch.cpp
@@ -1,15 +1,22 @@
 #include "pch.h"
+#include "../Http/AsyncSocket.h"
 #include "../Http/Http.h"
 #include "Dispatch.h"
 
 using namespace Http_Test::Dispatch;
 
 namespace {
-	auto onDispatch = [](ptr_t begin, ptr_t body, char* next, ptr_t end, TcpSocket& socket, Response& response) { return end; };
+	auto onDispatch = [](ptr_t begin, ptr_t body, char*& next, ptr_t end, TcpSocket& socket) {};
+	auto onDispatchAsync = [](ptr_t begin, ptr_t body, char*& next, ptr_t end, AsyncSocket& socket) -> Task<void> { co_return; };
 }
 
-std::function<char const* (ptr_t begin, ptr_t body, char* next, ptr_t end, TcpSocket& socket, Response& response)> Http_Test::Dispatch::OnDispatch = onDispatch;
+std::function<void(ptr_t begin, ptr_t body, char*& next, ptr_t end, TcpSocket& socket)> Http_Test::Dispatch::OnDispatch = onDispatch;
+std::function<Task<void>(ptr_t begin, ptr_t body, char*& next, ptr_t end, AsyncSocket& socket)> Http_Test::Dispatch::OnDispatchAsync = onDispatchAsync;
 
-char const* Dispatch(ptr_t begin, ptr_t body, char* next, ptr_t end, TcpSocket& socket, Response& response) {
-	return OnDispatch(begin, body, next, end, socket, response);
+void Dispatch(ptr_t begin, ptr_t body, char*& next, ptr_t end, TcpSocket& socket) {
+	OnDispatch(begin, body, next, end, socket);
+}
+
+Task<void> DispatchAsync(ptr_t begin, ptr_t body, char*& next, ptr_t end, AsyncSocket& socket) {
+	return OnDispatchAsync(begin, body, next, end, socket);
 }
