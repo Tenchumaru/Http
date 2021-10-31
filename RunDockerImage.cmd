f970fcdc93c41adef1c1c0dda192acd3b385882a
Tenchumaru <4085772+Tenchumaru@users.noreply.github.com>
2021-10-30 18:54:12 -0700

    Update RunDockerImage.cmd
    Change the name of the Docker image, include Yarborough, and run it
    privileged so it can run the debugger.

--- RunDockerImage.cmd
+++ RunDockerImage.cmd
@@ -1,10 +1,10 @@
 @ECHO OFF
 SETLOCAL
 
-SET IMAGE_NAME=clang-libssl
+SET IMAGE_NAME=development
 docker image inspect %IMAGE_NAME% > NUL 2> NUL
 IF ERRORLEVEL 1 (
 	ECHO %IMAGE_NAME% not available
 ) ELSE (
-	docker run -v C:\local:/local -v %~dp0:/Http %~1 -it %IMAGE_NAME%
+	docker run -v C:\local:/local -v %~dp0..\Yarborough:/Yarborough -v %~dp0:/Http --name Http -it --privileged %~1 %IMAGE_NAME%
 )
