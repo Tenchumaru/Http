0a99019475b43aca8055f4c9e8948bbed05cd7f1
Tenchumaru <4085772+Tenchumaru@users.noreply.github.com>
2023-09-02 12:59:33 -0700

    Attach to an extant Http container

--- RunDockerImage.cmd
+++ RunDockerImage.cmd
@@ -2,9 +2,18 @@
 SETLOCAL
 
 SET IMAGE_NAME=development
-docker image inspect %IMAGE_NAME% > NUL 2> NUL
+docker inspect Http > NUL
 IF ERRORLEVEL 1 (
-	ECHO %IMAGE_NAME% not available
+	docker image inspect %IMAGE_NAME% > NUL 2> NUL
+	IF ERRORLEVEL 1 (
+		ECHO %IMAGE_NAME% not available
+	) ELSE (
+		docker run -v C:\local:/local -v %~dp0..\Yarborough:/Yarborough -v %~dp0:/Http --name Http -it --privileged %~1 %IMAGE_NAME%
+	)
 ) ELSE (
-	docker run -v C:\local:/local -v %~dp0..\Yarborough:/Yarborough -v %~dp0:/Http --name Http -it --privileged %~1 %IMAGE_NAME%
+	docker inspect Http | find "Status" | find "running" > NUL
+	IF ERRORLEVEL 1 (
+		docker start Http
+	)
+	docker attach Http
 )
