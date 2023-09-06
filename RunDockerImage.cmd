e69969345832dc11cf46fab340d03f7553f03d72
Tenchumaru <4085772+Tenchumaru@users.noreply.github.com>
2023-09-06 08:43:17 -0700

    Improve RunDockerImage.cmd
    It now uses an environment variable to match the same-named command
    script in the Yarborough project.

--- RunDockerImage.cmd
+++ RunDockerImage.cmd
@@ -1,19 +1,20 @@
 @ECHO OFF
 SETLOCAL
 
+SET CONTAINER_NAME=Http
 SET IMAGE_NAME=development
-docker inspect Http > NUL
+docker inspect %CONTAINER_NAME% > NUL
 IF ERRORLEVEL 1 (
 	docker image inspect %IMAGE_NAME% > NUL 2> NUL
 	IF ERRORLEVEL 1 (
 		ECHO %IMAGE_NAME% not available
 	) ELSE (
-		docker run -v C:\local:/local -v %~dp0..\Yarborough:/Yarborough -v %~dp0:/Http --name Http -it --privileged %~1 %IMAGE_NAME%
+		docker run -v C:\local:/local -v %~dp0..\Yarborough:/Yarborough -v %~dp0:/Http --name %CONTAINER_NAME% -it --privileged %~1 %IMAGE_NAME%
 	)
 ) ELSE (
-	docker inspect Http | find "Status" | find "running" > NUL
+	docker inspect %CONTAINER_NAME% | find "Status" | find "running" > NUL
 	IF ERRORLEVEL 1 (
-		docker start Http
+		docker start %CONTAINER_NAME%
 	)
-	docker attach Http
+	docker attach %CONTAINER_NAME%
 )
